4dd2968ee5849882dfff3c5a4063e6ed70c7fbee
Jeff Ching <chingor@google.com>
2018-02-22 13:23:35 -0800

    Add environment variable for running the test with opcache enabled

--- testapps/tests/functional/bootstrap.php
+++ testapps/tests/functional/bootstrap.php
@@ -5,13 +5,19 @@ require __DIR__ . '/../../vendor/autoload.php';
 $host = getenv('HOST') ?: 'localhost';
 $port = (int)(getenv('PORT') ?: 9000);
 
+$opcache = getenv('ENABLE_OPCACHE') == '1'
+    ? '-dzend_extension=opcache.so -dopcache.enable=1'
+    : '';
+
 $command = sprintf(
-    'php -S %s:%d -t web -dauto_prepend_file=prepend.php -dextension=stackdriver_debugger.so >/dev/null 2>&1 & echo $!',
+    'php -S %s:%d -t web -dauto_prepend_file=prepend.php -dextension=stackdriver_debugger.so %s >/dev/null 2>&1 & echo $!',
     $host,
-    $port
+    $port,
+    $opcache
 );
 
 $output = [];
+printf('Starting web server with command: %s' . PHP_EOL, $command);
 exec($command, $output);
 $pid = (int) $output[0];
 
