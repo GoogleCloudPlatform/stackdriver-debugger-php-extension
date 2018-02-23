f8724deea6267af54a51e459f54578d725abe992
Jeff Ching <chingor@google.com>
2018-02-23 13:09:09 -0800

    Fix composer installer script to use curl

--- scripts/install_composer.sh
+++ scripts/install_composer.sh
@@ -18,7 +18,7 @@ set -xe
 
 echo "Installing composer"
 
-EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
+EXPECTED_SIGNATURE=$(curl -f https://composer.github.io/installer.sig)
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
 
